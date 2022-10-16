622cedad9f6cda2d09a9ab30d1505e60f9d06218
はよ日本語アプデ来いよ <96415345+Tomokundao@users.noreply.github.com>
2022-10-17 00:35:40 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 window.onload = function(){
-document.getElementById("loading").style.display ="none";
+	document.getElementById("loading").style.display = "none";
+	clock();
 }
 function load_newtab(url){
 	window.open(url);
@@ -7,4 +8,13 @@ function load_newtab(url){
 function load_thistab(url){
 	location.href = url;
 	loading.style.display = "block";
+}
+function clock(){
+	var realtime = new Date();
+	var realhour = realtime.getHours();
+	var realminutes = realtime.getMinutes();
+	var clocktime = realhour + ":" + realminutes;
+	document.getElementById("time").innerHTML = clocktime;
+	setInterval('clock()',10);
+
 }
