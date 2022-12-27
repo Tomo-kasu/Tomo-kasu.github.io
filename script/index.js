f29501dd5525775e1d67f001959aba7fa86f3ae5
はよ日本語アプデ来いよ <96415345+Tomokundao@users.noreply.github.com>
2022-12-28 01:29:22 +0900

    Create index.js

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 window.onload = function(){
 	document.getElementById("loading").style.display = "none";
+	clock();
 }
 function load_newtab(url){
 	window.open(url);
@@ -7,4 +8,17 @@ function load_newtab(url){
 function load_thistab(url){
 	location.href = url;
 	loading.style.display = "block";
+}
+function clock(){
+	var realtime = new Date();
+	var realhour = realtime.getHours();
+	var realminutes = realtime.getMinutes();
+	if(realminutes.length < 2){
+		var clocktime = realhour + ":" + realminutes;
+	}else{
+		var clocktime = realhour + ":0" + realminutes;
+	}
+	document.getElementById("time").innerHTML = clocktime;
+	setInterval('clock()',10);
+
 }
