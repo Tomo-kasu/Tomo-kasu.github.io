3447659375b416eb781250f8b0fe10c1ddc86391
はよ日本語アプデ来いよ <96415345+Tomokundao@users.noreply.github.com>
2022-12-28 01:32:59 +0900

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,6 +1,5 @@
 window.onload = function(){
 	document.getElementById("loading").style.display = "none";
-	clock();
 }
 function load_newtab(url){
 	window.open(url);
@@ -9,16 +8,4 @@ function load_thistab(url){
 	location.href = url;
 	loading.style.display = "block";
 }
-function clock(){
-	var realtime = new Date();
-	var realhour = realtime.getHours();
-	var realminutes = realtime.getMinutes();
-	if(realminutes.length < 2){
-		var clocktime = realhour + ":" + realminutes;
-	}else{
-		var clocktime = realhour + ":0" + realminutes;
-	}
-	document.getElementById("time").innerHTML = clocktime;
-	setInterval('clock()',10);
 
-}
