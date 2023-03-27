4f6d7a91c38acef7bd394c01af6137288f8cf980
はよ日本語アプデ来いよ <96415345+Tomokundao@users.noreply.github.com>
2023-03-27 20:37:50 +0900

    Update index.js

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@ function loaded(){
 	document.getElementById("loading").style.display = "none";
 }
 function load_newtab(url){
-	window.open(url);
+	window.open(url, '_blank');
 }
 function load_thistab(url){
 	location.href = url;
