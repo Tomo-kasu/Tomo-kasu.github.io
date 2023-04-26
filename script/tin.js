663e04e180e42ac6d3b3799c9f93ffd7f72e40d2
はよ日本語アプデ来いよ <96415345+Tomo-kasu@users.noreply.github.com>
2023-04-26 16:31:19 +0900

    Update tin.js

--- script/tin.js
+++ script/tin.js
@@ -1,6 +1,12 @@
 var iei = document.getElementById("iei");
+var n = document.getElementById("nhk");
+var t = 0;
+var f = 0;
+
 function tin(){
-        var TIN = new Audio('..\Image\\tin.mp3')
+
+        var TIN = new Audio('Image\\tin.mp3')
       TIN.play();
+      t + 33;
+      f + 4;     
 }
-touchPannel.addEventListener("touchstart",tin);
